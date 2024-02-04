ebc23ee359cdb15b4605f90fba987ca2bd4323dd
RivasJorge15 <rivas.jorge44@yahoo.com>
2024-02-04 17:58:54 -0600

    3

--- Contact List/src/Components/ContactList.jsx
+++ Contact List/src/Components/ContactList.jsx
@@ -12,14 +12,18 @@ export default function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts)
     useEffect(() => {
         async function fetchContacts() {
-            try {
-                fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
-            } catch (error) {
-                console.error(error);
-            }
+          try {
+            const response = await fetch(
+              "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
         }
-        fetchContacts()
-    }, [])
+        fetchContacts();
+      }, []);
 
     console.table(contacts)
 
